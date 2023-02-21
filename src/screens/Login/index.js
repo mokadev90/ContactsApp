2c43c1775d28deef5c2d6bfcdfc31bb6bd665dea
mokadev90 <romeramatias@gmail.com>
2023-02-21 17:55:51 -0300

    Style Login Screen

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,27 +1,9 @@
-import React, {useState} from 'react';
-import {Text, View} from 'react-native';
-import Container from '../../components/common/Container';
-import Input from '../../components/common/Input';
+import {useState} from 'react';
+import LoginComponent from '../../components/Login';
 
 const Login = () => {
   const [value, setValue] = useState('');
-  return (
-    <Container>
-      <Input
-        label="Username"
-        onChangeText={text => setValue(text)}
-        value={value}
-        // error={'This field is required'}
-      />
-      <Input
-        label="Password"
-        onChangeText={text => setValue(text)}
-        value={value}
-        icon={<Text>HIDE</Text>}
-        iconPosition="right"
-      />
-    </Container>
-  );
+  return <LoginComponent />;
 };
 
 export default Login;
