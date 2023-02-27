6c178dc50c950966636dd34f396afc4ae96af301
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:20:28 -0300

    Improve style and logic in Login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,9 +1,47 @@
-import {useState} from 'react';
-import LoginComponent from '../../components/Login';
+import {useNavigation, useRoute} from '@react-navigation/native'
+import {useContext, useEffect, useState} from 'react'
+import LoginComponent from '../../components/Login'
+import login from '../../context/actions/auth/login'
+import {GlobalContext} from '../../context/Provider'
 
 const Login = () => {
-  const [value, setValue] = useState('');
-  return <LoginComponent />;
-};
+  const [form, setForm] = useState({})
+  const [justSignedUp, setJustSignedUp] = useState(false)
+  const {params} = useRoute()
 
-export default Login;
+  useEffect(() => {
+    if (params?.data) {
+      setJustSignedUp(true)
+      setForm({...form, username: params.data.username})
+    }
+  }, [])
+
+  const {
+    authDispatch,
+    authState: {loading, error},
+  } = useContext(GlobalContext)
+
+  const onSubmit = () => {
+    if (form.username && form.password) {
+      login(form)(authDispatch)
+    }
+  }
+
+  const onChange = ({name, value}) => {
+    setJustSignedUp(false)
+    setForm({...form, [name]: value})
+  }
+
+  return (
+    <LoginComponent
+      onChange={onChange}
+      onSubmit={onSubmit}
+      form={form}
+      error={error}
+      loading={loading}
+      justSignedUp={justSignedUp}
+    />
+  )
+}
+
+export default Login
