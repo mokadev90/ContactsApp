ff11315095f9faaefa5a6677a9518b220f3dc775
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:20:35 -0300

    Improve style and logic in Register

--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -1,82 +1,92 @@
-import {useContext, useEffect, useState} from 'react';
-import RegisterComponent from '../../components/Register';
-import register from '../../context/actions/auth/register';
-import {GlobalContext} from '../../context/Provider';
-import axiosInstance from '../../helpers/axiosInterceptor';
+import {useFocusEffect, useNavigation} from '@react-navigation/native'
+import {useCallback, useContext, useEffect, useState} from 'react'
+import RegisterComponent from '../../components/Register'
+import {LOGIN} from '../../constants/routeNames'
+import register, {clearAuthState} from '../../context/actions/auth/register'
+import {GlobalContext} from '../../context/Provider'
+import axiosInstance from '../../helpers/axiosInterceptor'
 
 const Register = () => {
-  const [form, setForm] = useState({});
-  const [errors, setErrors] = useState({});
+  const [form, setForm] = useState({})
+  const {navigate} = useNavigation()
+  const [errors, setErrors] = useState({})
   const {
     authDispatch,
     authState: {loading, error, data},
-  } = useContext(GlobalContext);
+  } = useContext(GlobalContext)
 
-  useEffect(() => {
-    axiosInstance.post('/auth/login').catch();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        if (data || error) {
+          clearAuthState()(authDispatch)
+        }
+      }
+    }, [data, error]),
+  )
 
   const onChange = ({name, value}) => {
-    setForm({...form, [name]: value});
+    setForm({...form, [name]: value})
 
     if (value !== '') {
       if (name === 'password') {
         if (value.length < 6) {
           setErrors(prev => {
-            return {...prev, [name]: 'This field need min 6 characters'};
-          });
+            return {...prev, [name]: 'This field need min 6 characters'}
+          })
         } else {
           setErrors(prev => {
-            return {...prev, [name]: null};
-          });
+            return {...prev, [name]: null}
+          })
         }
       } else {
         setErrors(prev => {
-          return {...prev, [name]: null};
-        });
+          return {...prev, [name]: null}
+        })
       }
     } else {
       setErrors(prev => {
-        return {...prev, [name]: 'This field is required'};
-      });
+        return {...prev, [name]: 'This field is required'}
+      })
     }
-  };
+  }
 
   const onSubmit = () => {
-    console.log('form: ', form);
     if (!form.username) {
       setErrors(prev => {
-        return {...prev, username: 'Please add a Username'};
-      });
+        return {...prev, username: 'Please add a Username'}
+      })
     }
     if (!form.firstName) {
       setErrors(prev => {
-        return {...prev, firstName: 'Please add a First name'};
-      });
+        return {...prev, firstName: 'Please add a First name'}
+      })
     }
     if (!form.lastName) {
       setErrors(prev => {
-        return {...prev, lastName: 'Please add a Last name'};
-      });
+        return {...prev, lastName: 'Please add a Last name'}
+      })
     }
     if (!form.email) {
       setErrors(prev => {
-        return {...prev, email: 'Please add a Email'};
-      });
+        return {...prev, email: 'Please add a Email'}
+      })
     }
     if (!form.password) {
       setErrors(prev => {
-        return {...prev, password: 'Please add a Password'};
-      });
+        return {...prev, password: 'Please add a Password'}
+      })
     }
-  };
 
-  if (
-    Object.values(form).length === 5 &&
-    Object.values(form).every(item => item.trim().length > 0) &&
-    Object.values(form).every(item => !item)
-  ) {
-    register(form)(authDispatch);
+    if (
+      Object.values(form).length === 5 &&
+      Object.values(form).every(item => item.trim().length > 0) &&
+      Object.values(errors).every(item => !item)
+    ) {
+      register(form)(authDispatch)(response => {
+        navigate(LOGIN, {data: response})
+      })
+    }
   }
 
   return (
@@ -88,7 +98,7 @@ const Register = () => {
       error={error}
       loading={loading}
     />
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
