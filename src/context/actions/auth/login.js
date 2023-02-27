572d0ed0b1a3fd576f3bb9ada6bdac2e5c2c5304
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:15:21 -0300

    Create login action

---
+++ src/context/actions/auth/login.js
@@ -0,0 +1,35 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {
+  LOGIN_FAIL,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+} from '../../../constants/actionTypes'
+import axiosInstance from '../../../helpers/axiosInterceptor'
+
+export default ({password, username}) =>
+  dispatch => {
+    dispatch({type: LOGIN_START})
+    axiosInstance
+      .post('/auth/login', {
+        password,
+        username,
+      })
+      .then(async res => {
+        console.log('user ', res.data.user)
+        console.log('user ', JSON.stringify(res.data.user))
+        await AsyncStorage.setItem('token', res.data.token)
+        await AsyncStorage.setItem('user', JSON.stringify(res.data.user))
+        const user = await AsyncStorage.getItem('user')
+        console.log('user en storage ', user)
+        dispatch({type: LOGIN_SUCCESS, payload: res.data})
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch({
+          type: LOGIN_FAIL,
+          payload: err.response
+            ? err.response.data
+            : {error: 'Something went wrong'},
+        })
+      })
+  }
