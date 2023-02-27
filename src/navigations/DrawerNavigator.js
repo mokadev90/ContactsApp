b7854d814d9fa3f59b2bc47d64672095554ae38c
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:18:44 -0300

    Improve Drawer Navigator

--- src/navigations/DrawerNavigator.js
+++ src/navigations/DrawerNavigator.js
@@ -1,16 +1,32 @@
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import React from 'react';
-import {Text, View} from 'react-native';
-import {HOME_NAVIGATOR} from '../constants/routeNames';
-import HomeNavigator from './HomeNavigator';
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import React, {useContext} from 'react'
+import {Image, SafeAreaView, Text, View} from 'react-native'
+import colors from '../assets/theme/colors'
+import Container from '../components/common/Container'
+import {HOME_NAVIGATOR} from '../constants/routeNames'
+import {GlobalContext} from '../context/Provider'
+import HomeNavigator from './HomeNavigator'
+import SideMenu from './SideMenu'
+
+const getDrawerContent = (navigation, authDispatch) => {
+  return <SideMenu navigation={navigation} authDispatch={authDispatch} />
+}
 
 const DrawerNavigator = () => {
-  const Drawer = createDrawerNavigator();
+  const Drawer = createDrawerNavigator()
+  const {authDispatch} = useContext(GlobalContext)
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerType="slide"
+      drawerContent={({navigation}) =>
+        getDrawerContent(navigation, authDispatch)
+      }
+      drawerStyle={{backgroundColor: colors.darkBlue}}
+      screenOptions={{headerShown: false}}>
       <Drawer.Screen name={HOME_NAVIGATOR} component={HomeNavigator} />
     </Drawer.Navigator>
-  );
-};
+  )
+}
 
-export default DrawerNavigator;
+export default DrawerNavigator
