21c84fbdf54db84f97d73bb93d2ff6e770e0c81f
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:16:55 -0300

    Add onSuccess logic in Register action

--- src/context/actions/auth/register.js
+++ src/context/actions/auth/register.js
@@ -1,10 +1,16 @@
 import {
+  CLEAR_AUTH_STATE,
   REGISTER_FAIL,
   REGISTER_LOADING,
   REGISTER_SUCCESS,
-} from '../../../constants/actionTypes';
-import axiosInstance from '../../../helpers/axiosInterceptor';
+} from '../../../constants/actionTypes'
+import axiosInstance from '../../../helpers/axiosInterceptor'
 
+export const clearAuthState = () => dispatch => {
+  dispatch({
+    type: CLEAR_AUTH_STATE,
+  })
+}
 export default ({
     email,
     password,
@@ -12,8 +18,9 @@ export default ({
     firstName: first_name,
     lastName: last_name,
   }) =>
-  dispatch => {
-    dispatch({type: REGISTER_LOADING});
+  dispatch =>
+  onSuccess => {
+    dispatch({type: REGISTER_LOADING})
     axiosInstance
       .post('/auth/register', {
         email,
@@ -23,15 +30,15 @@ export default ({
         last_name,
       })
       .then(res => {
-        dispatch({type: REGISTER_SUCCESS, payload: res.data});
+        dispatch({type: REGISTER_SUCCESS, payload: res.data})
+        onSuccess(res.data)
       })
       .catch(err => {
-        console.log(err);
         dispatch({
           type: REGISTER_FAIL,
           payload: err.response
             ? err.response.data
             : {error: 'Something went wrong'},
-        });
-      });
-  };
+        })
+      })
+  }
