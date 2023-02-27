ba00058054c47a8eb4bf69786d9e49e02b1ce034
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:17:59 -0300

    Add login and logout actions in reducer

--- src/context/reducers/authReducer.js
+++ src/context/reducers/authReducer.js
@@ -1,26 +1,49 @@
 import {
+  CLEAR_AUTH_STATE,
+  LOGIN_FAIL,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  LOGOUT,
   REGISTER_FAIL,
   REGISTER_LOADING,
   REGISTER_SUCCESS,
-} from '../../constants/actionTypes';
+} from '../../constants/actionTypes'
 
 const authReducer = (state, {type, payload}) => {
   switch (type) {
-    case 'LOGIN':
-      return {};
-
+    case LOGIN_START:
     case REGISTER_LOADING:
-      return {...state, loading: true};
+      return {...state, loading: true}
+
+    case LOGIN_SUCCESS:
+      return {...state, loading: false, data: payload, isLoggedIn: true}
 
     case REGISTER_SUCCESS:
-      return {...state, loading: false, error: payload};
+      return {...state, loading: false, data: payload}
 
+    case LOGIN_FAIL:
     case REGISTER_FAIL:
-      return {...state, loading: false, data: payload};
+      return {...state, loading: false, error: payload}
+
+    case CLEAR_AUTH_STATE:
+      return {
+        ...state,
+        loading: false,
+        data: null,
+        error: null,
+      }
+
+    case LOGOUT:
+      return {
+        ...state,
+        loading: false,
+        data: null,
+        isLoggedIn: false,
+      }
 
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default authReducer;
+export default authReducer
