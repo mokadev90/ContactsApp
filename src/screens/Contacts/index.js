d0c416a51323b915cbf1c66d4c92ca5de2a6b346
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:20:14 -0300

    Improve style and login in Contacts

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import {Text} from 'react-native';
-import Container from '../../components/common/Container';
+import {useNavigation} from '@react-navigation/native'
+import React, {useContext, useEffect, useState} from 'react'
+import {Text, TouchableOpacity} from 'react-native'
+import Container from '../../components/common/Container'
+import Icon from '../../components/common/Icon'
+import ContactsComponent from '../../components/ContactsComponent'
+import getContacts from '../../context/actions/contacts/getContacts'
+import {GlobalContext} from '../../context/Provider'
 
 const Contacts = () => {
-  return (
-    <Container>
-      <Text>Hi from Contacts</Text>
-    </Container>
-  );
-};
+  const {setOptions, toggleDrawer} = useNavigation()
+  const [modalVisible, setModalVisible] = useState(false)
 
-export default Contacts;
+  const {
+    contactsState: {
+      getContacts: {data, loading},
+    },
+    contactsDispatch,
+  } = useContext(GlobalContext)
+
+  useEffect(() => {
+    getContacts()(contactsDispatch)
+  }, [])
+
+  useEffect(() => {
+    setOptions({
+      headerLeft: () => (
+        <TouchableOpacity
+          onPress={() => {
+            console.log('toogle')
+            toggleDrawer()
+          }}>
+          <Icon
+            type="material"
+            size={26}
+            style={{padding: 10, paddingLeft: 0, color: 'black'}}
+            name="menu"
+          />
+        </TouchableOpacity>
+      ),
+    })
+  }, [])
+
+  return (
+    <ContactsComponent
+      modalVisible={modalVisible}
+      setModalVisible={setModalVisible}
+      data={data}
+      loading={loading}
+    />
+  )
+}
+
+export default Contacts
