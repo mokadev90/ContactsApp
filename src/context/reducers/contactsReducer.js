724ad423a058cf5b099652839ca9801eab3dcc38
mokadev90 <romeramatias@gmail.com>
2023-02-27 18:18:19 -0300

    Add getContacts action in reducer

--- src/context/reducers/contactsReducer.js
+++ src/context/reducers/contactsReducer.js
@@ -1,11 +1,45 @@
+import {
+  GET_CONTACTS_FAIL,
+  GET_CONTACTS_LOADING,
+  GET_CONTACTS_SUCCESS,
+} from '../../constants/actionTypes'
+
 const contactsReducer = (state, {type, payload}) => {
   switch (type) {
-    case 'GET_CONTACTS':
-      return {};
+    case GET_CONTACTS_LOADING:
+      return {
+        ...state,
+        getContacts: {
+          ...state.getContacts,
+          loading: true,
+          error: null,
+        },
+      }
+
+    case GET_CONTACTS_SUCCESS:
+      return {
+        ...state,
+        getContacts: {
+          ...state.getContacts,
+          loading: false,
+          data: payload,
+          error: null,
+        },
+      }
+
+    case GET_CONTACTS_FAIL:
+      return {
+        ...state,
+        getContacts: {
+          ...state.getContacts,
+          loading: false,
+          error: payload,
+        },
+      }
 
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default contactsReducer;
+export default contactsReducer
