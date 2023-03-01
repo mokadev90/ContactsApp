700eb0a186360da6e40a1ab5db87c505a1308775
mokadev90 <romeramatias@gmail.com>
2023-03-01 00:31:01 -0300

    Add createContact logic in reducer

--- src/context/reducers/contactsReducer.js
+++ src/context/reducers/contactsReducer.js
@@ -1,8 +1,12 @@
 import {
+  CREATE_CONTACTS_FAIL,
+  CREATE_CONTACTS_LOADING,
+  CREATE_CONTACTS_SUCCESS,
   GET_CONTACTS_FAIL,
   GET_CONTACTS_LOADING,
   GET_CONTACTS_SUCCESS,
 } from '../../constants/actionTypes'
+import getContacts from '../actions/contacts/getContacts'
 
 const contactsReducer = (state, {type, payload}) => {
   switch (type) {
@@ -11,6 +15,16 @@ const contactsReducer = (state, {type, payload}) => {
         ...state,
         getContacts: {
           ...state.getContacts,
+          loading: true,
+          error: null,
+        },
+      }
+
+    case CREATE_CONTACTS_LOADING:
+      return {
+        ...state,
+        createContact: {
+          ...state.createContact,
           loading: true,
           error: null,
         },
@@ -27,11 +41,38 @@ const contactsReducer = (state, {type, payload}) => {
         },
       }
 
+    case CREATE_CONTACTS_SUCCESS:
+      return {
+        ...state,
+        createContact: {
+          ...state.createContact,
+          loading: false,
+          data: payload,
+          error: null,
+        },
+        getContacts: {
+          ...state.getContacts,
+          loading: false,
+          data: [payload, ...state.getContacts.data],
+          error: null,
+        },
+      }
+
     case GET_CONTACTS_FAIL:
       return {
         ...state,
         getContacts: {
           ...state.getContacts,
+          loading: false,
+          error: payload,
+        },
+      }
+
+    case CREATE_CONTACTS_FAIL:
+      return {
+        ...state,
+        createContact: {
+          ...state.createContact,
           loading: false,
           error: payload,
         },
