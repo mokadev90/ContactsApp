97ac74dc19173f86be6ce60c8ff5f4a45f3bd2d5
mokadev90 <romeramatias@gmail.com>
2023-03-01 00:31:44 -0300

    Add full logic in Settings

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,12 +1,113 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, {useEffect, useState} from 'react'
+import {Text, View} from 'react-native'
+import SettingsComponents from '../../components/SettingsComponents'
 
 const Settings = () => {
-  return (
-    <View>
-      <Text>Hi from Settings</Text>
-    </View>
-  );
-};
+  const [email, setEmail] = useState(null)
+  const [modalVisible, setModalVisible] = useState(false)
+  const [sortBy, setSortBy] = useState(null)
 
-export default Settings;
+  const saveSetting = ({key, value}) => {
+    AsyncStorage.setItem(key, value)
+  }
+
+  const settingsOptions = [
+    {
+      title: 'My Info',
+      subtitle: 'Setup your profile',
+      onPress: () => {},
+    },
+    {
+      title: 'Accounts',
+      subtitle: null,
+      onPress: () => {},
+    },
+    {
+      title: 'Default account for new contacts',
+      subtitle: email,
+      onPress: () => {},
+    },
+    {
+      title: 'Contacts to display',
+      subtitle: 'All contacts',
+      onPress: () => {},
+    },
+    {
+      title: 'Sort by',
+      subtitle: sortBy,
+      onPress: () => {
+        setModalVisible(true)
+      },
+    },
+    {
+      title: 'Name format',
+      subtitle: 'First name first',
+      onPress: () => {},
+    },
+    {
+      title: 'Import',
+      subtitle: null,
+      onPress: () => {},
+    },
+    {
+      title: 'Export',
+      subtitle: null,
+      onPress: () => {},
+    },
+    {
+      title: 'Blocked numbers',
+      subtitle: null,
+      onPress: () => {},
+    },
+    {
+      title: 'About Contacts',
+      subtitle: null,
+      onPress: () => {},
+    },
+  ]
+
+  const prefArray = [
+    {
+      name: 'First name',
+      selected: sortBy === 'First name',
+      onPress: () => {
+        saveSetting('sortBy', 'First Name')
+        setSortBy('First name')
+        setModalVisible(false)
+      },
+    },
+    {
+      name: 'Last name',
+      selected: sortBy === 'Last name',
+      onPress: () => {
+        saveSetting('sortBy', 'Last Name')
+        setSortBy('Last name')
+        setModalVisible(false)
+      },
+    },
+  ]
+
+  const getSettings = async () => {
+    const user = await AsyncStorage.getItem('user')
+    setEmail(JSON.parse(user).email)
+
+    const sortPref = await AsyncStorage.getItem('sortBy')
+    if (sortPref) setSortBy(sortPref)
+  }
+
+  useEffect(() => {
+    getSettings()
+  }, [])
+
+  return (
+    <SettingsComponents
+      modalVisible={modalVisible}
+      setModalVisible={setModalVisible}
+      settingsOptions={settingsOptions}
+      prefArray={prefArray}
+    />
+  )
+}
+
+export default Settings
