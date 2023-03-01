7c23d824316364563ddd7b5201902ab94871538f
mokadev90 <romeramatias@gmail.com>
2023-03-01 00:31:20 -0300

    Add full logic in CreateContact

--- src/screens/CreateContact/index.js
+++ src/screens/CreateContact/index.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import {useNavigation} from '@react-navigation/native'
+import React, {createRef, useContext, useRef, useState} from 'react'
 import {Text, View} from 'react-native'
 import CreateContactComponent from '../../components/CreateContactComponent'
+import {CONTACT_LIST} from '../../constants/routeNames'
+import createContact from '../../context/actions/contacts/createContact'
+import {GlobalContext} from '../../context/Provider'
 
 const CreateContact = () => {
-  return <CreateContactComponent />
+  const {
+    contactsDispatch,
+    contactsState: {
+      createContact: {loading, error, data},
+    },
+  } = useContext(GlobalContext)
+  const [form, setForm] = useState({})
+  const {navigate} = useNavigation()
+  const sheetRef = useRef(null)
+  const [localFile, setLocalFile] = useState(null)
+
+  const openSheet = () => {
+    if (sheetRef.current) {
+      sheetRef.current.open()
+    }
+  }
+
+  const closeSheet = () => {
+    if (sheetRef.current) {
+      sheetRef.current.close()
+    }
+  }
+
+  const onChangeText = ({name, value}) => {
+    setForm({...form, [name]: value})
+  }
+
+  const onSubmit = () => {
+    createContact(form)(contactsDispatch)(() => {
+      navigate(CONTACT_LIST)
+    })
+  }
+
+  const toogleValueChange = () => {
+    setForm({...form, isFavorite: !form.isFavorite})
+  }
+
+  const onFileSelected = image => {
+    closeSheet()
+    setLocalFile(image)
+    console.log('image ', image)
+  }
+
+  return (
+    <CreateContactComponent
+      form={form}
+      setForm={setForm}
+      onSubmit={onSubmit}
+      onChangeText={onChangeText}
+      loading={loading}
+      error={error}
+      toogleValueChange={toogleValueChange}
+      sheetRef={sheetRef}
+      openSheet={openSheet}
+      closeSheet={closeSheet}
+      onFileSelected={onFileSelected}
+      localFile={localFile}
+    />
+  )
 }
 
 export default CreateContact
