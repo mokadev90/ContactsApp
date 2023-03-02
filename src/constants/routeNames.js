6a160d1412b56c12f89ef439e47bb3d52234d08a
mokadev90 <romeramatias@gmail.com>
2023-03-02 13:06:48 -0300

    Apply format

--- src/constants/routeNames.js
+++ src/constants/routeNames.js
@@ -1,7 +1,8 @@
-export const CONTACT_LIST = 'Contacts';
-export const HOME_NAVIGATOR = 'Home';
-export const CREATE_CONTACT = 'Create Contact';
-export const CONTACT_DETAIL = 'Contact detail';
-export const SETTINGS = 'Settings';
-export const LOGIN = 'Login';
-export const REGISTER = 'Register';
+export const CONTACT_LIST = 'Contacts'
+export const HOME_NAVIGATOR = 'Home'
+export const CREATE_CONTACT = 'Create Contact'
+export const CONTACT_DETAIL = 'Contact detail'
+export const SETTINGS = 'Settings'
+export const LOGIN = 'Login'
+export const REGISTER = 'Register'
+export const LOGOUT = 'Logout'
