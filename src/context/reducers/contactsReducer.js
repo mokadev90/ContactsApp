c7f68ae238c5c0e4ecfc97fb0d148419ba9242db
mokadev90 <romeramatias@gmail.com>
2023-03-03 11:20:06 -0300

    Add delete and edit actions in reducer

--- src/context/reducers/contactsReducer.js
+++ src/context/reducers/contactsReducer.js
@@ -2,6 +2,12 @@ import {
   CREATE_CONTACTS_FAIL,
   CREATE_CONTACTS_LOADING,
   CREATE_CONTACTS_SUCCESS,
+  DELETE_CONTACT_FAIL,
+  DELETE_CONTACT_LOADING,
+  DELETE_CONTACT_SUCCESS,
+  EDIT_CONTACT_LOADING,
+  EDIT_CONTACT_SUCCESS,
+  EDIT_CONTACT_FAIL,
   GET_CONTACTS_FAIL,
   GET_CONTACTS_LOADING,
   GET_CONTACTS_SUCCESS,
@@ -21,6 +27,26 @@ const contactsReducer = (state, {type, payload}) => {
       }
 
     case CREATE_CONTACTS_LOADING:
+      return {
+        ...state,
+        createContact: {
+          ...state.createContact,
+          loading: true,
+          error: null,
+        },
+      }
+
+    case DELETE_CONTACT_LOADING:
+      return {
+        ...state,
+        deleteContact: {
+          ...state.deleteContact,
+          loading: true,
+          error: null,
+        },
+      }
+
+    case EDIT_CONTACT_LOADING:
       return {
         ...state,
         createContact: {
@@ -58,6 +84,44 @@ const contactsReducer = (state, {type, payload}) => {
         },
       }
 
+    case DELETE_CONTACT_SUCCESS:
+      return {
+        ...state,
+        deleteContact: {
+          ...state.deleteContact,
+          loading: false,
+          error: null,
+        },
+        getContacts: {
+          ...state.getContacts,
+          loading: false,
+          data: state.getContacts.data.filter(item => item._id !== payload),
+          error: null,
+        },
+      }
+
+    case EDIT_CONTACT_SUCCESS:
+      return {
+        ...state,
+        createContact: {
+          ...state.createContact,
+          loading: false,
+          error: null,
+        },
+        getContacts: {
+          ...state.getContacts,
+          loading: false,
+          data: state.getContacts.data.map(item => {
+            if (item._id === payload._id) {
+              return payload
+            } else {
+              return item
+            }
+          }),
+          error: null,
+        },
+      }
+
     case GET_CONTACTS_FAIL:
       return {
         ...state,
@@ -69,6 +133,26 @@ const contactsReducer = (state, {type, payload}) => {
       }
 
     case CREATE_CONTACTS_FAIL:
+      return {
+        ...state,
+        createContact: {
+          ...state.createContact,
+          loading: false,
+          error: payload,
+        },
+      }
+
+    case DELETE_CONTACT_FAIL:
+      return {
+        ...state,
+        deleteContact: {
+          ...state.deleteContact,
+          loading: false,
+          error: null,
+        },
+      }
+
+    case EDIT_CONTACT_FAIL:
       return {
         ...state,
         createContact: {
